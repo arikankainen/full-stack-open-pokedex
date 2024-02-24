b08f8cca7021785b1a2892290889bddc917ff256
Ari Kankainen <ari.kankainen@saunalahti.fi>
2024-02-24 16:27:20 +0200

    added periodic health route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/periodic_health', (req, res) => {
   // res.send('ok')
   res.status(500).send('Internal Server Error')
 })
